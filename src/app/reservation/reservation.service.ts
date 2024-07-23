e8712d13d26fdd1e997a35f30ed59d3349b78311
DESKTOP-DFT1TDC\Rohit <adarshjasrotia14@gmail.com>
2024-07-23 16:09:30 +0530

    add styles

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,6 +8,11 @@ export class ReservationService {
 
   private reservations:Reservation[]=[];
 
+  constructor(){
+    let savedReservations=localStorage.getItem("reservations");
+    this.reservations=savedReservations?JSON.parse(savedReservations):[];
+  }
+
   //CRUD 
 
   getReservations():Reservation[]{
@@ -19,16 +24,22 @@ export class ReservationService {
   }
 
   addReservation(reservation:Reservation):void{
+
+    reservation.id=Date.now().toString();
     this.reservations.push(reservation);
+    console.log(this.reservations);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
   }
 
   deleteReservation(id:string):void{
     let index=this.reservations.findIndex(res=>res.id===id)
     this.reservations.splice(index,1)
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
   }
 
-  updateReservation(updatedReservation:Reservation):void{
-    let index=this.reservations.findIndex(res=>res.id===updatedReservation.id)
+  updateReservation(id:string, updatedReservation:Reservation):void{
+    let index=this.reservations.findIndex(res=>res.id===id)
     this.reservations[index]=updatedReservation
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
   }
 }
